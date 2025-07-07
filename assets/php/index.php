e03017d0babd3c82b72bfe635c5df8e502075c55
Alamgir hossain <alamgirabc6@gmail.com>
2025-07-07 12:40:52 +0600

    upload

--- assets/php/index.php
+++ assets/php/index.php
@@ -8,14 +8,18 @@ echo"Hello PHP";
 
 $name = "Akib";
 
-*/
+
 $num1 = 10;
 $num2 = 5;
 $total_number = $num1 + $num2;
 echo "$total_number";
 
+*/
+
+function address(){
+    echo"This is my Address.";
+}
+
+address();
 
 
-
-
-?>

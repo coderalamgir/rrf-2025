4dca8345c3bb3c2cbc90e40221dfe02bec3cd8ec
alamgir <alamgirabc6@gmail.com>
2025-07-08 19:09:08 +0600

    Update comparison.php

--- assets/php/operators/comparison.php
+++ assets/php/operators/comparison.php
@@ -29,3 +29,14 @@ var_dump($greater_or_equal);
 
 $less_or_equal = $a <= $b; 
 var_dump($less_or_equal);
+
+//২টি সংখ্যার মধ্যে তুলনা করতে হবে।
+$number1 = 10;
+$number2 = 20;
+
+if($number1 < $number2) {
+    echo" $number1 is less than $number2 ";
+}else{
+    echo" $number1 is not less than $number2 ";
+}
+

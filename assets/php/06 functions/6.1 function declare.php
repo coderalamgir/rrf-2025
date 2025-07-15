f0b09bf83b52fbd8ad50c506078076a7495f3181
alamgir <alamgirabc6@gmail.com>
2025-07-15 08:55:33 +0600

    function

---
+++ assets/php/06 functions/6.1 function declare.php
@@ -0,0 +1,18 @@
+<?php
+ //  আলোচ্য বিষয়ঃ
+ /*
+    >> Regular Function
+    >> Paramerer, Argument যুক্ত ফাংশন।
+    >> Return Valu
+    >> Anonymous Function
+    >> Arrow Function
+    >> Callback Function
+    >> Variable Function
+ */
+
+    function add(){
+        $result = 5 + 5;
+        echo" $result ";
+    }
+
+   echo add();

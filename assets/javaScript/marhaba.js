eff54885d4958510973a177d1f5471cd8f7487fd
alamgir <alamgirabc6@gmail.com>
2025-08-06 08:41:57 +0600

    variables

---
+++ assets/javaScript/marhaba.js
@@ -0,0 +1,99 @@
+"use strict"
+
+
+/*
+    // Number Datatype এর ব্যবহার।
+    const age = 42;
+    // alert(age);
+    console.log(age);
+
+    const booksToRead = 2;
+    console.log(booksToRead);
+
+
+    const wakeUpTime = 6;
+    
+    console.log(wakeUpTime);
+    const wastedTime = 4;
+    console.log(wastedTime);
+
+    const borrow = 80000;
+    console.log(borrow);
+
+    const goals = 3;
+    alert(goals);
+
+    const pillow = 2;
+    // console.log(pillow);
+    alert(pillow);
+
+    
+   ///////////////// String Datatype এর ব্যবহার।
+   const favFood = "Biryani";
+    console.log(favFood);
+    const drinking = "Tea";
+    console.log(drinking);
+
+    const subject = "English";
+    console.log(subject);
+
+    const teacher = "Dragon Delowar";
+    console.log(teacher);
+
+    const phone = 'Sumsung';
+    console.log(phone);
+
+    const fruits = ' WaterMelon';
+    console.log(fruits);
+
+    const fname = "Alamgir" ;
+    console.log(fname);
+    
+    const address = "Name: Alam", vill = "Hakimpur", post = "Kanchon Nagar", Thana = "Daulatpur", District = "Kushtia";
+    console.log(address);
+    console.log(vill);
+    console.log(post);
+    console.log(Thana);
+    console.log(District);
+    const bestFriendStudy = "Kushtia government college";
+    //alert(bestFriendStudy);
+    const fan = " Bipasha Basu ";
+    console.log(fan);
+
+    const movieOfTheYear = 'Masti';
+    console.log(movieOfTheYear);
+    const hobby = "travel";;
+    console.log(hobby);
+
+    const color = "Blue";
+    console.log(color);
+    */
+   // stirng জোড়া//////////////////////////////।
+
+   const income = 100;
+   const cost = 40;
+   console.log(income - cost);
+
+   const sugar = 30;
+   const daddy = 20;
+   console.log(sugar + daddy);
+
+   const hasPencil = 10;
+   const givenPencil = 2;
+   console.log(hasPencil / givenPencil);
+
+   const text1 = "Hello";
+   const text2 = "World";
+   console.log(text1 + " " + text2);
+
+   const firstPart = "Bangla";
+   const secondPart = "desh";
+   console.log(firstPart + secondPart);
+
+   const Number = 50;
+   const get = 9;
+   console.log(Number % get);
+
+
+
+

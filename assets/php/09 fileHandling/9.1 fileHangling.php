a75682c18f19c3636e4f11b22de569b1fd4e74ca
Alamgir hossain <alamgirabc6@gmail.com>
2025-08-11 13:28:47 +0600

    file Handling

---
+++ assets/php/09 fileHandling/9.1 fileHangling.php
@@ -0,0 +1,23 @@
+<?php
+    declare(strict_types = 1);
+
+    /*
+
+    $file = fopen("new.txt", "w");
+    fwrite($file, "Projukti plus");
+
+    $file = fopen("new.txt", "r");
+    echo fread($file, filesize("new.txt"));
+    fclose($file);
+
+
+    $file = fopen("new2.txt", "w"); // এখানে, new2.txt হলো ফাইল এর নাম এবং w হলো ফাইল মোডস অর্থাৎ লেখার জন্য।
+    fwrite($file, "We are learning PHP about file & folder "); //$file এর অর্থ হলো এই ভ্যারিয়েবল এর মধ্যে যে ফাইল আছে তার মধ্যে  We are learning PHP about file & folder এই টেক্সট লিখলাম।
+    $file = fopen("new2.txt", "r");  // "r" এর অর্থ হলো ফাইলটি পড়ার মুড এ আছে।
+    echo fread($file, filesize("new2.txt")); //এখানে বলা হয়েছে ফাইলটি echo এর মাধ্যমে প্রিন্ট করা হলো।
+    fclose($file);  // এর অর্থ হলো ফাইলটি বন্ধ করা হয়েছে।
+    $another_file = fopen("another.txt", "w");
+    rename("another.txt", "Changed.txt");
+    mkdir("my_folder");
+
+    */

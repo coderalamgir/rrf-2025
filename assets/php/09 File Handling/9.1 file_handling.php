f78707fd00dc2f2c0b980e3a40168c637ee03dd4
alamgir <alamgirabc6@gmail.com>
2025-08-12 07:21:50 +0600

    date function

--- assets/php/09 File Handling/9.1 file_handling.php
+++ assets/php/09 File Handling/9.1 file_handling.php
@@ -1,4 +1,6 @@
 <?php
+
+/*
     declare(strict_types = 1);
     // date(); function এর ব্যবহার।
     echo date("d/m/y") . "\n";
@@ -7,3 +9,14 @@
     echo date("Y")  . "\n";
     echo date("h:i:sa")  . "\n";
     date_default_timezone_set("America/New_York");
+    */
+
+    $fileHandle = fopen("user_activitz_log.txt", "a");
+    if ( $fileHandle ) {
+        $logData = "User: JohnDoe | Login Time:" . date('Y-m-d H:i:s') . " | Status: Success \n";
+        fwrite( $fileHandle, $logData );
+        fclose( $fileHandle );
+        echo " User activitz logged successfully. ";
+    } else {
+        echo " Failed to create or open log file. ";
+    }

7326724164e0a095f0795caea89aec0e975bc569
alamgir <alamgirabc6@gmail.com>
2025-08-13 00:15:13 +0600

    create dir

--- assets/php/09 File Handling/9.1 file_handling.php
+++ assets/php/09 File Handling/9.1 file_handling.php
@@ -9,7 +9,7 @@
     echo date("Y")  . "\n";
     echo date("h:i:sa")  . "\n";
     date_default_timezone_set("America/New_York");
-    */
+
 
     $fileHandle = fopen("user_activitz_log.txt", "a");
     if ( $fileHandle ) {
@@ -20,3 +20,30 @@
     } else {
         echo " Failed to create or open log file. ";
     }
+
+    $fileHandle1 = fopen("log1.txt", "a");
+    fwrite($fileHandle1, "শেষে নতুন ডাটা যোগকরা হয়েছে।");
+    $read = fread($fileHandle1, filesize("log1.txt"));
+    fclose($fileHandle1);
+    echo"$read";
+
+
+    $fileLog = fopen("log3.txt", "a");
+    if ( $fileLog ) {
+        $logData2 = "User Login";
+        fwrite($fileLog, $logData2);
+
+    }
+    */
+    $myFolder = ("projukti_pro");
+
+    if( !is_dir($myFolder) ) {
+        mkdir($myFolder);
+        echo" Folder is created successfully.";
+    }else {
+        echo "Sorry, \"$myFolder\" Folder is already Exist.";
+    }
+
+    //এখানে, !is_dir($myFolder) অর্থ হলো যদি projuktiplus নামে ফোল্ডার না থাকে।
+    // mkdir($myFolder); তাহলে ফোল্ডারটি তৈরি কর। তারপর দেখাও Folder is created successfully.
+    // অন্যথায় বলবে Sorry, Folder is already Exist.

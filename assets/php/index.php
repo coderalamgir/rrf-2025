f4cfa90496a7c2d755681cafb8efba8375606f65
alamgir <alamgirabc6@gmail.com>
2025-08-16 21:14:56 +0600

    Update index.php

--- assets/php/index.php
+++ assets/php/index.php
@@ -1,59 +1,74 @@
 <?php
 /*
-echo"Hello PHP";
+    echo"Hello PHP";
 
 
 
 
 
-$name = "Akib";
+    $name = "Akib";
 
 
-$num1 = 10;
-$num2 = 5;
-$total_number = $num1 + $num2;
-echo "$total_number";
+    $num1 = 10;
+    $num2 = 5;
+    $total_number = $num1 + $num2;
+    echo "$total_number";
 
 
 
-function address(){
-    echo"This is my Address.";
-}
+    function address(){
+        echo"This is my Address.";
+    }
 
-address();
+    address();
 
-$my_name // Snake_case
-$myName // camelCase
-$My_Name // Pascal_Case
+    $my_name // Snake_case
+    $myName // camelCase
+    $My_Name // Pascal_Case
 
-// php তে constant লেখার নিয়ম। ২ভাবে constant লেখা যায়। 
+    // php তে constant লেখার নিয়ম। ২ভাবে constant লেখা যায়। 
 
-১। define() ফাংশন এর মাধ্যমে এবং
-    define(" constant এর নাম বড় হাতের অক্ষরে লিখতে হয়। ", ভ্যালু);
-    define( "DAYS IN WEEK", 7 );
-    
-    echo"There are " . DAYS_IN_WEEK . " days in a week";
+    ১। define() ফাংশন এর মাধ্যমে এবং
+        define(" constant এর নাম বড় হাতের অক্ষরে লিখতে হয়। ", ভ্যালু);
+        define( "DAYS IN WEEK", 7 );
+        
+        echo"There are " . DAYS_IN_WEEK . " days in a week";
 
 
-২। const কিওয়ার্ড দিয়ে। যেমন-
+    ২। const কিওয়ার্ড দিয়ে। যেমন-
+        const DAYS_IN_WEEK = 7;
+        echo"There are " . DAYS_IN_WEEK . " days in a week";
+        
+
+
+
     const DAYS_IN_WEEK = 7;
-    echo"There are " . DAYS_IN_WEEK . " days in a week";
-    
+        echo"There are " . DAYS_IN_WEEK . " days in a week";
 
 
+        define( "VATE_RATE" , 0.15);
+        $product_price = 100;
+        $total_price = $product_price + ($product_price * VATE_RATE);
+        echo" Total price including vat " . $total_price;
 
-const DAYS_IN_WEEK = 7;
-    echo"There are " . DAYS_IN_WEEK . " days in a week";
+
+    //পাই এর মান।
+    define("PI", 3.1415);
+    $radius = 5;
+    $circumference = 2 * PI * $radius;
+    echo" circumference of the circle " . $circumference ;
 
 
-    define( "VATE_RATE" , 0.15);
-    $product_price = 100;
-    $total_price = $product_price + ($product_price * VATE_RATE);
-    echo" Total price including vat " . $total_price;
+    $txt1 = "Learn PHP";
+    $txt2 = "w3School.com";
+
+    // print "<h1>$txt1</h1>\n";
+    // print "<h1> " . $txt2 . " </h1>";
+
+    print_r($txt1);
+
 */
 
-//পাই এর মান।
-define("PI", 3.1415);
-$radius = 5;
-$circumference = 2 * PI * $radius;
-echo" circumference of the circle " . $circumference ;
+    // $x = 5;
+    // $y = "My name is Akib";
+    // var_dump($y);

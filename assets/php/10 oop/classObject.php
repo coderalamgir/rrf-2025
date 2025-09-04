40f895f3109feeeb3ec989a9cc352bc1d91109b2
Alamgir hossain <alamgirabc6@gmail.com>
2025-09-04 12:50:47 +0600

    66

--- assets/php/10 oop/classObject.php
+++ assets/php/10 oop/classObject.php
@@ -317,7 +317,61 @@
     // object এর মধ্যে প্রবেশ।
     //echo $product_1->name;      // এখানে $product_1->name অংশটুকু সম্পূর্ন variable হিসাবে বিবেচিত হবে।
     echo $product_1->showDetails();
-    ***********/ 
+
+    <?php
+    /*
+    // ক্লাস  হলো একটি কাঠামো যা দিয়ে অনেকগুলো অবজেক্ট তৈরি করা যাই।
+    class Product {
+        $name = "Mobile";   // $name হলো একটি ভ্যারিয়েবল কিন্তু ক্লাসের মধ্যে থাকলে তাকে প্রোপার্টিজ বলে।
+        $brand = "Samsung";
+    }
+
+
+    class product{
+        // properties
+        public $name = "Samsung";
+        public $price = "400";
+
+        //method
+        public function showDetails(){
+            echo "Name: $this -> name, Price: $this -> price"  ;
+        }
+    }
+
+    //Object তৈরি
+    $product_1 = new product();
+
+    //object এ প্রবেশ
+    echo $product_1 -> name;
+    echo $product_1 -> price;
+
+
+    //class তৈরি করি
+    class Product{
+        //properties তৈরি করি।
+        public $name = "Mobile";
+        public $price = "2000";
+        public $brand;
+        public $cagegory;
+        public $stock;
+        public $SKU;
+    }
+
+    //method তৈরি করি।
+    //     public function showDetails(){
+    //         echo "Name: $this->name, price: $this->price";
+    //     }
+ 
+
+    //Object তৈরি করি।
+    $product_2 = new Product();
+
+    //Object এ প্রবেশ করি।
+    echo "$product_2->name \n";
+    echo $product_2->price ;
+    echo $product_2->name;
+    
+
 
     //class
     class car{
@@ -340,3 +394,4 @@
     echo $myCar->model = "Toyota";
     echo"\n";
     echo $myCar->start();
+    ***********/

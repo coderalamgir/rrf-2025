d8eef676e7303f8e5a2b2cdb6a6b514f5c5655a4
alamgir <alamgirabc6@gmail.com>
2025-09-06 18:26:17 +0600

    Update 10.3 constractor.php

--- assets/php/10 OOp/10.3 constractor.php
+++ assets/php/10 OOp/10.3 constractor.php
@@ -5,7 +5,7 @@
         public $name;
         public $age;
 
-        //class এর মধ্যে constructor তৈরি করি। auto call করার জন্য যে ফাংশন আছে তার নাম --constructor.
+        //class এর মধ্যে constructor তৈরি করি। auto call করার জন্য যে ফাংশন আছে তার নাম --constructor. এখানে parameter (Variable) গুলো দিয়ে দিতে হয়।
         public function __construct($studentName, $studentAge) {
             $this->name = $studentName;
             $this->age = $studentAge;
@@ -19,7 +19,7 @@
     }
 
     //Object তৈরি করি।
-    $student1 = new person("Akib Islam", 4);
+    $student1 = new person("Akib Islam ", 4);  // এখানে Arguments (Value বা মান) গুলো দিয়ে দিতে হয়।
 
 
     // মান দেখাতে।

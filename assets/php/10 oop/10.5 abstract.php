bab9b8a6550053255672e2ddde84b2af9195af0b
alamgir <alamgirabc6@gmail.com>
2025-09-13 06:38:38 +0600

    Update 10.5 abstract.php

--- assets/php/10 oop/10.5 abstract.php
+++ assets/php/10 oop/10.5 abstract.php
@@ -1,4 +1,6 @@
 <?php
+
+    /*
     abstract class technologyVillage{
         public $tutorial;
 
@@ -17,3 +19,40 @@
 
     $php = new phptuto('PHP'); 
     echo $php->message();
+    */ 
+
+
+    //Base abstract class
+    abstract class paymentGateWay{
+        abstract public function pay($amount); // abstract method এর body বা { } থাকে না।
+    }
+
+    // Child abstract class paypal এবং extends কীওয়ার্ড এর মাধ্যমে ক্লাসেকে inherit করলাম।
+    class paypal extends paymentGateWay{
+        public function pay($amount){
+            echo "Paid $$amount via Paypal.\n";
+        }
+    }
+
+    // Child abstract class stripeএবং extends কীওয়ার্ড এর মাধ্যমে ক্লাসেকে inherit করলাম।
+    class stripe extends paymentGateWay{
+        public function pay($amount){
+            echo "Paid $$amount via Stripe.\n";    
+        }
+    }
+
+    //Bkash payment gateway 
+    class bkash extends paymentGateWay{
+        public function pay($amount) {
+            echo"Paid $$amount via Bkash.";
+        }
+    }
+
+    $paypal = new paypal();
+    $paypal->pay(100);
+
+    $stripe = new stripe();
+    $stripe->pay(900);
+
+    $bkash = new bkash();
+    $bkash->pay(300);

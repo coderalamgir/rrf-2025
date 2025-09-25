f01d29c87aaaec5a822ca591a78a877d7de040d4
Alamgir hossain <alamgirabc6@gmail.com>
2025-09-25 13:25:47 +0600

    function repeat

--- assets/php/05 loops/5.3 do..while loop.php
+++ assets/php/05 loops/5.3 do..while loop.php
@@ -21,3 +21,11 @@
         echo"Give your password. \n";
         $i++;
     }while ( $i <= 3 );
+
+
+
+    $i = 1;
+    do{
+        echo"Login attempt $i\n";
+        $i++;
+    }while( $i <= 6 );
---
+++ assets/php/06 functions/6.1 function.php
@@ -0,0 +1,13 @@
+<?php
+    // অতিসাধারণ একটি ফাংশন এখানে প্রকাশ করা হলো।
+    function greet (  ) {
+        echo"Hello, Future Developers. \n";
+    }
+
+    greet();
+
+    //return function with parameters & arguments.
+    function add ( $n1, $n2 ) {
+        return $n1 + $n2;
+    }
+    echo add(10, 15);

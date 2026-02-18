ccb102830495ecf4e2c780d9e9645804f3551a48
alamgir <alamgirabc6@gmail.com>
2026-02-18 19:35:39 +0600

    anisul

--- assets/javaScript/03 output.js
+++ assets/javaScript/03 output.js
@@ -3,7 +3,34 @@
     ৪ ভাবে জাভাস্ক্রিপ্ট কোড দেখানো যায়।
 
         ১। console.log() এর মাধ্যমে 
-        ২। ‍alert() এর মাধ্যমে।
+        ২। ‍alert() এর মাধ্যমে। 
         ৩। document.write()
-
 */
+
+// let myName = "Akib Islam";
+// console.log(myName);
+
+// দুই ভাবে ভ্যারিয়েবল ডিক্লেয়ার করা যায়।
+    // ১।
+    //   let name, age, city, country;
+    // ২।
+    //  let name;
+    //  let age;
+    //  let city;
+    //  let country;
+
+    // console.log("Personal Budget Tracker App.");
+    // console.log(" 18000");  //My income
+    // console.log(" 8000");  //my expense
+
+
+    const userName = "Akib Islam";
+    const income = 18000;
+    const expense = 10000;
+    console.log(userName);
+    console.log(income);
+    console.log(expense);
+
+    console.log(typeof(income));
+    console.log(typeof(userName));
+    

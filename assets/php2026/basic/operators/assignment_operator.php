cf8df3b8d2b410fd4c7bdec99e0ad3362673ccea
alamgir <alamgirabc6@gmail.com>
2026-02-19 17:41:00 +0600

    operator

---
+++ assets/php2026/basic/operators/assignment_operator.php
@@ -0,0 +1,45 @@
+<?php 
+  //! Assingment Operator
+  // equal sign
+  $age = 18;
+  echo $age;
+  echo "\n";
+  
+  //? += plus assign sign
+  $age1 = 18;
+  $age1 += 5;
+  echo "New age: $age1";
+  echo "\n";
+
+// -= minus assign sign
+  $age2 = 20;
+  $age2 -= 2;
+  echo "Your age is now: $age2 ";
+  echo "\n";
+
+  // *= multiply assign
+  $age3 = 30;
+  $age3 *= 3;
+  echo "Your age after two multiply: $age3";
+  echo "\n";
+
+  //? /= divided assign
+  $number1 = 30;
+  $number1 /= 2;
+  echo "Number1 value after divided: $number1";
+  echo "\n";
+
+//? %= modulas assign
+  $modulas = 10;
+  $modulas %= 2;
+  echo "Modulas: $modulas";
+  echo "\n";
+
+//? .= string concatination
+  $str = "Hello";
+  $str .= " World !";
+  echo "String Concatination: $str";
+
+
+
+

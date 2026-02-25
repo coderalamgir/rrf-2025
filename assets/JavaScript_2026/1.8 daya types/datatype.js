bb1241f99e9881638e6efd155097cc0dcdef1668
Alamgir hossain <alamgirabc6@gmail.com>
2026-02-25 13:09:14 +0600

    datatypes

--- assets/JavaScript_2026/1.8 daya types/datatype.js
+++ assets/JavaScript_2026/1.8 daya types/datatype.js
@@ -30,9 +30,46 @@
       2. Array
       3. Function (Object)
 
-
-
-
-
-
  */
+    // Object তৈরি।
+    let product = {
+        name: "Mobile",
+        price: 1800
+    }
+
+
+    //array তৈরি।
+    let fruits = ["Mango", " Banana", " Lychee", " Orange"];
+    //console.log(fruits[1]);
+    //document.write(fruits[2]);
+
+    // সম্পূর্ন এ্যারে দেখতে।
+    console.log(fruits);
+    
+    //নতুন এ্যারে আইটেম যোগ করতে।
+    fruits[2] = "Jackfruit";
+    console.log(fruits);
+    
+    let text = "ProjuktiPlus";
+    text[0] = ["P"];
+    console.log(text[0]);
+
+     // primitive datatype এর ভ্যালু পরিবর্তন করা যায় না। 
+     // তাই ভ্যালুর উপর ভিত্তি করে তুলনা করা হয়।
+    let numOne = 4;
+    let numTwo = 4;
+    console.log(numOne === numTwo);
+
+    //Non Primitive datatype এর ভ্যালু পরিবর্তন করা যায়।
+    //  তাই ভ্যালুর উপর ভিত্তি করে তুলনা করা হয় না।
+    //  তুলনা করা হয় রেফারেন্স এর উপর ভিত্তি করে ।
+    let userOne = {
+        name: "Akib",
+        country: "Bangladesh"
+    }
+     
+    let userTwo = {
+        name: "Akib",
+        country: "Bangladesh"
+    }
+    console.log(userOne === userTwo);

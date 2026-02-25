2ee697de7d81518f6b93122696d7d11bbccb3ee4
alamgir <alamgirabc6@gmail.com>
2026-02-25 08:58:25 +0600

    data types

---
+++ assets/JavaScript_2026/1.8 daya types/datatype.js
@@ -0,0 +1,38 @@
+
+
+/* 
+  Data Types
+    1. Primitive (Immutable - Value পরিবর্তন করা যায় না।)
+      1. Number ==> Integer, Floating point
+          let price = 50 or 50.6;
+
+      2. String ==> Text
+          let name = "Akib":
+
+      3. Boolean ==> true or false
+          let isStudent = true;
+
+      4. null ==> Empty Value
+          let amount = nul;
+
+      5. Undefined ==> Value not assigned
+          let address;
+
+      6. Symbol ==> Unique identifire
+          let uniqueId = Symbol("id");
+
+      7. BigInt ==> Very large integer
+          let bigNumber = 123458694568745684566446n;
+          
+
+    2. Non primitive (Mutable - Value পরিবর্তন করা যায়।)
+      1. Object
+      2. Array
+      3. Function (Object)
+
+
+
+
+
+
+ */

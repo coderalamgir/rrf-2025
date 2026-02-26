2414fd5a9051a3e0075feaeb7d4ac0512f1aabef
Alamgir hossain <alamgirabc6@gmail.com>
2026-02-26 14:21:00 +0600

    math function

---
+++ assets/JavaScript_2026/1.9 math_object/number.js
@@ -0,0 +1,32 @@
+
+
+let number = 4.5693;
+console.log(number);
+console.log(Math.round(number));
+console.log(Math.floor(number));
+console.log(Math.ceil(number));
+
+
+console.log(Math.max(-3, -4, 0, 2, 4));
+console.log(Math.min(-3, -4, 0, 2, 4));
+console.log(Math.abs(-10));
+
+
+console.log(Math.sign(-10));
+console.log(Math.sign(0));
+console.log(Math.sign(10));
+
+console.log(Math.sqrt(4));   //বর্গমূল
+console.log(Math.sqrt(100));  //বর্গমূল
+
+console.log(Math.cbrt(27));  //ঘনমূল
+console.log(Math.cbrt(125));  //ঘনমূল
+
+console.log(Math.pow(10, 2));  //power
+console.log(Math.pow(10, 3));  //power
+
+console.log(Math.pow(1));  //Log
+
+console.log(Math.sin(90 * (Math.PI / 180)));  // Radius value
+
+console.log(Math.floor(Math.random() * 11));  // Random value 

f6fa752031c548ed0dd5aae443989e53f7a86d35
alamgir <alamgirabc6@gmail.com>
2026-02-28 17:45:44 +0600

    Update 3.6 stringOperator.php

--- assets/php/03 operators/3.6 stringOperator.php
+++ assets/php/03 operators/3.6 stringOperator.php
@@ -2,7 +2,87 @@
 
 //concatinate বা জোড়া দিতে ‍string operator ব্যবহার করা হয়। ‍string operator এর অপর নাম concatinate operator.
 $first_name = "Akib";
-$last_name = "Islam";
+$last_name = "Islam \n";
 
 $full_name = $first_name . " " . $last_name ;
 echo"$full_name";
+
+$number1 = 10;
+$number2 = 20;
+if($number1 < $number2){
+  echo "$number1 is less than $number2.\n";
+  }else{
+  echo "$number1 is not less than $number2";
+}
+
+
+$age1 = 16;
+$age2 = 21;
+if($age1 < $age2){
+  echo"Age is less.\n";
+}else{
+  echo"Age is not less.\n";
+}
+
+
+$price1 = 200;
+$price2 = 400;
+if($price1 < $price2){
+  echo"Phone price is lower.\n";
+}else{
+  echo"Phone price is not lower.";
+}
+
+$firstNumber = 15;
+$secondNumber = 10;
+if($firstNumber > $secondNumber){
+  echo"$firstNumber is greater than $secondNumber \n";
+}else{
+  echo"$firstNumber is not greater than $secondNumber \n";
+}
+
+
+$score1 = 85;
+$score2 = 70;
+if($score1 > $score2){
+  echo"Score is higher. \n";
+}else{
+  echo"Score is not higher. \n";
+}
+
+$temp1 = 30;
+$temp2 = 25;
+if($temp1 > $temp2){
+  echo"Temperature is higher. \n";
+}else{
+  echo"Temperature is not higher. \n";
+}
+
+// < = Less than or equal to
+$bookPrice = 350;
+$budget = 300;
+if( $bookPrice <= $budget){
+  echo "Sadika will buy the book. \n";
+  }else{
+  echo "Sadika will not buy the book. \n";
+}
+
+
+$age = 18;
+$limit = 21;
+if( $age <= $limit ){
+  echo"You are within the age limit. \n";
+  }else{
+  echo"You are not within the age limit. \n";
+}
+
+$fee = 150;
+$budgets = 200;
+if($fee <= $budgets){
+  echo "Fee is within budget. \n";
+  }else{
+  echo "Fee is not within budget. \n";
+}
+
+// > = Greater than or Equal
+

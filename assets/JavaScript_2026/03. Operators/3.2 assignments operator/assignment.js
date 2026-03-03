997e16cea3c4561335823044b1dc3f34a311dd3b
Alamgir hossain <alamgirabc6@gmail.com>
2026-03-03 14:28:43 +0600

    operator

---
+++ assets/JavaScript_2026/03. Operators/3.2 assignments operator/assignment.js
@@ -0,0 +1,46 @@
+
+// JavaScript Assignment Operators
+/*
+
+    = 
+    +=
+    -=
+    *=
+    /=
+    %=
+
+
+*/
+
+let x = 10;
+let y =  4;
+
+x += 3;    //x = x + 3;
+console.log(x);
+
+x -= 3;    //x = x - 3;
+console.log(x);
+
+x *= 3;    //x = x * 3;
+console.log(x);
+
+x /= 3;    //x = x / 3;
+console.log(x);
+
+x %= 3;    //x = x % 3;
+console.log(x);
+
+x += y;    //x = x + y;
+console.log(x);
+
+x -= y;    //x = x - y;
+console.log(x);
+
+x *= y;    //x = x * y;
+console.log(x);
+
+x /= y;    //x = x / y;
+console.log(x);
+
+x %= y;    //x = x % y;
+console.log(x); 

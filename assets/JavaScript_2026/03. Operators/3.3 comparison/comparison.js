f3c20749edb00cd1ae20284a8a87dccfc4c4e66a
Alamgir hossain <alamgirabc6@gmail.com>
2026-03-04 13:35:21 +0600

    comparison

---
+++ assets/JavaScript_2026/03. Operators/3.3 comparison/comparison.js
@@ -0,0 +1,28 @@
+
+//JavaScript Comparison Operator
+/*
+    1.  ==      শুধুমাত্র মানের দিক দিয়ে সমান।
+    2.  !=      শুধুমাত্র মানের দিক দিয়ে সমান নয়।
+    3.  ===     ডাটা টাইপ ও মান উভয় দিক দিয়ে সমান।
+    4.  !==     ডাটা টাইপ ও মান উভয় দিক দিয়ে সমান নয়।
+    5.  >       বড়।
+    6.  <       ছোট।
+    7.  >=      বড অথবা সমান।
+    8.  <=      ছোট অথবা সমান।
+*/
+
+    console.log(3 == 2);
+    console.log(3 != 2);
+    console.log(2 == 2);
+    console.log(2 === 2);
+    console.log("2" === 2);
+    console.log(2 > 3);
+    console.log(3 >= 3);
+    console.log(2 < 3);
+    console.log(2 <= 3);
+    console.log(3 <= 3);
+
+    console.log(0 == false);
+    console.log(0 === false);
+    console.log(1 == true);
+    console.log(1 === true);

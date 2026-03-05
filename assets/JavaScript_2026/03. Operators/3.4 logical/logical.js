c332f13b04f6c813e57bca9981fd65dddb5d94d7
alamgir <alamgirabc6@gmail.com>
2026-03-05 20:02:13 +0600

    logical operator

---
+++ assets/JavaScript_2026/03. Operators/3.4 logical/logical.js
@@ -0,0 +1,20 @@
+
+//logical Operator
+
+// logical AND (&&)  operator
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+console.log(1 && 1);
+
+// Logical OR (||) operator
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+
+// Logical NOT (!) Operator
+console.log(!true);
+console.log(!false);

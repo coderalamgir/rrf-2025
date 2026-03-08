516febe19f95141398bd78b8f748a60c9e95c3dd
alamgir <alamgirabc6@gmail.com>
2026-03-08 21:47:32 +0600

    nested if else

---
+++ assets/JavaScript_2026/04 conditional statement/4.1 if else else if/4.1 if else elseif.js
@@ -0,0 +1,26 @@
+
+// ১টা শর্ত হলে।
+let num = 6;
+if( num > 0 ){
+  console.log(`${num} is a positive Number.`);
+}
+
+// ২টা শর্ত হলে।
+if( num > 0 ){
+  console.log(`${num} is a positive Number`);
+}else{
+  console.log(`${num} is a Negative Number`);
+}
+
+// ২টার বেশি শর্ত হলে।
+  let Number = -10;
+  
+if( Number > 0 ){
+  console.log(`${Number} is a positive Number.`);
+}else if( Number < 0 ){
+  console.log(`${Number} is a Negative Number.`);
+}else if(Number == 0){
+  console.log(`${Number}. It is zero.`);
+}else{
+  console.log(`${Number} It is not a number.`);
+}
---
+++ assets/JavaScript_2026/04 conditional statement/4.1 if else else if/4.2 nested if else/nested if else.js
@@ -0,0 +1,19 @@
+
+// Nested if else
+// একটা if else এর মধ্যে আরো if else থাকলে তাকে Nested if else বলে।
+
+let a = 100, b = 200, c = 30;
+
+if( a > b ){
+  if( a > c ){
+    console.log(`${a} is the maximum number.`);
+  }else{
+    console.log(`${c} is the maximum number.`);
+  }
+}else{
+  if( b > c ){
+    console.log(`${b} is the maximum number.`);
+  }else{
+    console.log(`${c} is the maximum number.`);
+  }
+}

60c713e9b1c86df00501a9a23e22c47f1c62dddf
alamgir <alamgirabc6@gmail.com>
2026-03-09 20:43:14 +0600

    test

--- assets/JavaScript_2026/1.5 connect js file/main.js
+++ assets/JavaScript_2026/1.5 connect js file/main.js
@@ -1,4 +1,6 @@
-const { x } = require("../1.6 variable/variable");
+
+/*
+    const { x } = require("../1.6 variable/variable");
 
     
     document.write("Hello JavaScript");
@@ -7,4 +9,16 @@ const { x } = require("../1.6 variable/variable");
     alert("Hello JavaScript in 2026");
     console.log("Look at the JavaScript in browser console.");
     document.getElementById("output").innerHTML = " JavaScript is a client side language ";document.write(x);
+*/
+
+function changeColor(akib){
+    akib.style.color = "red";
+    akib.style.fontSize = "40px";
+    
+}
+
+function restoreColor(akib){
+    akib.style.color = "black";
+    akib.style.fontSize = "32px";
+}
 

4ed305b4837242765f0d52e41c7f63a209f9c08f
alamgir <alamgirabc6@gmail.com>
2026-03-12 17:55:42 +0600

    a

---
+++ assets/JavaScript_2026/05 arrays/array.js
@@ -0,0 +1,47 @@
+
+
+// Array
+/*
+const vasitable = ["Potato", "Onion", "Brinjal", "Radish", "Carrot", "Tomato", "Ladish Finger", "Law", "Cucumber", "Potol"]; 
+
+let size = vasitable.length;
+console.log(vasitable.length);
+
+
+const favouriteMovies = ["Bipasha", "Dena Pawna", "Jibon niye khela", "Mouchak", "Rajkumari"];
+
+const color = ["Black", "White", "Blue", "Green"];
+let sizes = color.length;
+console.log(color.length);
+
+const capitals = ["Dhaka", "Kathmandu", "Dilhi", "Karachi", "Kampala", "Nirobi"];
+let sizess = capitals.length;
+console.log(sizess);
+
+
+// find out index number
+let indexing = capitals[5];
+console.log(indexing);
+
+const mobileGames = ["Freefire", "Pubzi", "Candy crush", "Temple run"];
+let geme = mobileGames[3];
+console.log(mobileGames[3]);
+
+
+// index element update.
+mobileGames[3] = "subway surfar";
+console.log(mobileGames);
+
+
+const numbers = [ 71, 72, 73, 74, 75, 76, 77, 78,79];
+// numbers.push(80, 81,82, 83, 84,85);
+// const pop1 = numbers.Pop();
+// console.log(pop1);
+// console.log(numbers);
+
+
+*/
+const fruits = ["Apple", "Banana", "Orange", "Mango", "Goava"];
+// fruits.shift();
+fruits.unshift("Jackfruit");
+console.log(fruits);

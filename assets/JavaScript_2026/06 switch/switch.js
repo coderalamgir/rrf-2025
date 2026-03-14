c38d9e9225ba221afc1372e3d62172ccb78b4058
alamgir <alamgirabc6@gmail.com>
2026-03-14 23:43:02 +0600

    boolean

--- assets/JavaScript_2026/06 switch/switch.js
+++ assets/JavaScript_2026/06 switch/switch.js
@@ -1,6 +1,8 @@
 
 
 //Switch case
+
+/*
 let orderStatus = "  shipped  ";  
 
 switch(orderStatus.trim()){
@@ -21,4 +23,41 @@ switch(orderStatus.trim()){
     break;
   default:
     console.log("Unknown");
+}
+    */
+
+let marks = parseInt(prompt("Enter your marks."));
+
+switch(true){
+  case(marks >= 80 && marks <= 100):
+  console.log("Your result A+");
+  break;
+
+  case(marks >= 70 && marks <= 79):
+  console.log("Your result A");
+  break;
+
+  case(marks >= 60 && marks <= 69):
+  console.log("Your result A-");
+  break;
+
+  case(marks >= 50 && marks <= 59):
+  console.log("Your result B");
+  break;
+
+  case(marks >= 40 && marks <= 49):
+  console.log("Your result C");
+  break;
+
+  case(marks >= 33 && marks <= 39):
+  console.log("Your result D");
+  break;
+
+  case(marks >= 0 && marks <= 32):
+  console.log("Your result F");
+  break;
+
+  default:
+  console.log("Invalid input. Please enter number 0 to 100 than try again.");
+    
 }

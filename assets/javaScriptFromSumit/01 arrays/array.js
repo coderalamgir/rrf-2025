a4539b0212802cc05f3b833e86111df7d5f2c12f
Alamgir hossain <alamgirabc6@gmail.com>
2026-03-15 11:24:17 +0600

    change folder name

---
+++ assets/javaScriptFromSumit/01 arrays/array.js
@@ -0,0 +1,21 @@
+
+//array
+// const cars = ["RFL", "Saab", "BMW", "Volvo"];
+// let x = cars[2];
+// console.log(cars[2]);
+// document.getElementById("demo").innerHTML = cars;
+
+// const cars = ["Saab", "Volvo", "BMW"];
+// document.getElementById("demo").innerHTML = cars;
+
+
+//object
+// const person = {firstName: "Akib", lastName: "Islam", age:5};
+
+// //access object
+// console.log(person.firstName);
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let length = fruits.length;
+console.log(length);
+console.log(fruits[0]);
+console.log(fruits[fruits.length -1]);

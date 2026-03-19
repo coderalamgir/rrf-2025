cf086ec87816a2e024017aa6c6bef89032a6b779
alamgir <alamgirabc6@gmail.com>
2026-03-19 22:59:49 +0600

    function

---
+++ assets/JavaScript_2026/04 conditional statement/4.8  do while loop/doWhile.js
@@ -0,0 +1,11 @@
+
+
+
+//Do While loop
+
+let correctPassword = "12345"
+let userInput;
+do{
+  userInput =  prompt(" Give your correct password.");
+}while( userInput != correctPassword );
+console.log("Login Successful.");
---
+++ assets/javaScriptFromSumit/04 Function/function.js
@@ -0,0 +1,100 @@
+
+
+//JavaScript Function
+  function fanOffKor1(){
+    document.getElementById("demo").innerHTML = "You have clicked on FIRST BUTTON.";
+  
+  }
+
+  function fanOffKor2(){
+    document.getElementById("demo").innerHTML = "You have clicked on SECOND BUTTON.";
+  }
+
+  function changeColor(){
+    document.getElementById("demoh").innerHTML.style.color = "red";
+  }
+
+
+  function fanOffKor(){
+    console.log("বসা থেকে উঠে দাড়া।");
+    console.log("ফ্যান এর সুইচ এর দেকে যাও।");
+    console.log("ফ্যান এর সুইচ বন্ধ কর।");
+  }
+
+  fanOffKor();
+
+  // জুস খাও নামে একটা ফাংশন লিখ। আউটপুট দিবে “এই নাও লেবুর জুস ” ।
+  function juceKhao(){
+    console.log("এই নাও লেবুর জুস ।");
+  }
+
+  juceKhao();
+
+  function myRoutine(){
+    console.log("Brush my teeth.");
+    console.log("Wash my face.");
+    console.log("I have breakfast.");
+    console.log("I finish my lesson.");
+  }
+  myRoutine();
+
+  function myFamilyMembers(){
+    console.log("Akib is my son.");
+    console.log("Sadika is my daughter.");
+    console.log("Popy is my wife.");
+  }
+  myFamilyMembers();
+
+  function taskDone(){
+    console.log("I have finished PHP.");
+    console.log("I have finished JavaScript.");
+    console.log("I have finished CSS.");
+    console.log("I have finished HTML.");
+    console.log("I have finished git.");
+  }
+  taskDone();
+
+  function greetMe(){
+    console.log("Good Mornigh! Have a great day!");
+  }
+  greetMe();
+
+
+  function getFavFriends(){
+    console.log("Shofiq, Hasan, Rafiul");
+  }
+  getFavFriends();
+
+  function introduceMyself(){
+    console.log("My name is Alamgir Hossain, age 42, phone 01715684469, ");
+  }
+
+  introduceMyself();
+
+  function doubleIt(number){
+    const doubled = number * 2;
+    console.log(doubled);
+  }
+
+  doubleIt(10);
+  doubleIt(100);
+  doubleIt(1000);
+  doubleIt(9);
+
+
+  function square(number){
+    const borgo = number * number;
+    console.log("Square of number is:" + borgo);
+  }
+
+  square(4);
+  square(12);
+  square(25);
+  square(10);
+
+  function add(num1, num2){
+    const sum = num1 + num2;
+    console.log(sum);
+  }
+
+  add(5, 71);

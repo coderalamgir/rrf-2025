b32cdca1e81028331e0b6249b4131bc7ccf21310
Alamgir hossain <alamgirabc6@gmail.com>
2026-04-04 13:38:59 +0600

    discount

--- assets/JavaScript_2026/04 conditional statement/4.1 if else else if/4.3 nested logical/nested logical.js
+++ assets/JavaScript_2026/04 conditional statement/4.1 if else else if/4.3 nested logical/nested logical.js
@@ -1,6 +1,8 @@
 
 // Nested if else with Logical operator
 
+
+/*
 let a = 100, b = 200, c = 300;
 
 if( a > b && a > c ){
@@ -10,3 +12,98 @@ if( a > b && a > c ){
 }else{
     console.log(`${c} is the maximum Number.`);
 }
+
+
+const salary = 66000;
+const isBCS = true;
+if(salary > 55000 && isBCS == false){
+    console.log("সেই লেভেলের পাত্র।");
+}else{
+    console.log("দুরে গিয়ে মরো।");
+}
+
+const salary = 26000;
+const height = 68;
+
+if(salary > 25000 || height == 72){
+    console.log("Bolo baba kobul.");
+}else{
+    console.log("Vag tui mokbul");
+}
+ 
+
+const age = 19;
+const height = 64;
+
+if( age > 18  && height > 60 ){
+   console.log("তুমি গাড়ি ঠেলো।"); 
+}else{
+    console.log("তুমি গাড়িতে বসো।");
+}
+
+    const mathScore = 83;
+    const englishScore = 79;
+    if( mathScore > 80 && englishScore > 85  ){
+        console.log("You are eligible for addmision.");
+    }else{
+        console.log("You are ready for marry.");
+    }
+
+    const gpa = 5;
+    const familyIncome = 10500;
+
+    if( gpa == 5 && familyIncome < 10000 ){
+        console.log(" You have gotten scholership.");
+    }else{
+        console.log(" You have to study with education fees. ");
+    }
+
+    const age = 28;
+    const experience = 3;
+    if(age < 30 && experience > 2){
+        console.log("You are allowed to seat for exam.");
+    } else{
+        console.log("You are not allowed to seat for exam.");
+    }
+
+
+    const eggs = 14;
+    const chicken = 0;
+    if(eggs > 12 || chicken == 0){
+        console.log("You will cock egg corma.");
+    }else{
+        console("You will eat banana  & bun.");
+    }
+
+    const bodyTemparature = 102;
+    const isCough = true;
+    if( bodyTemparature > 100 || isCough == true ){
+        console.log("He will go to the doctor.");
+    }else{
+        console.log("He will lay with Katha.");
+    }
+
+
+    const studentPresence = 90;
+    const homeWork = true;
+    if(studentPresence > 80 && homeWork == true){
+        console.log("You are eligible for Exam.");
+    }else{
+        console.log("You are not eligible for Exam.");
+    }
+
+    const electricity = false;
+    const mobileCharge = false;
+    if( electricity == false && mobileCharge == false ){
+        console.log("You will now read.");
+    }else{
+        console.log("You will play video game.");
+    }
+
+    const shirtPrice = 1300;
+    const discount = 20;
+    if( shirtPrice > 1000 && discount == 20 ){
+        console.log("You will get discount.");
+    }
+*/
+    const price = 6500;

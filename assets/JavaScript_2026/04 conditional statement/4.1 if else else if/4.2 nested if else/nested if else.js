2c6d7b0753469e263442940d5255774766de74fc
alamgir <alamgirabc6@gmail.com>
2026-04-10 17:12:25 +0600

    function

--- assets/JavaScript_2026/04 conditional statement/4.1 if else else if/4.2 nested if else/nested if else.js
+++ assets/JavaScript_2026/04 conditional statement/4.1 if else else if/4.2 nested if else/nested if else.js
@@ -77,9 +77,56 @@ if(movieTime <= "9pm"){
   }else{
     console.log("Next 5 years single thako.");
   }
- */  
+ 
   const salary = 63000;
   const isBCS = true;
   if( salary >= 50000 && isBCS == true){
-    
+    console.log("আমি রাজি, আমি রাজি.....");
   }
+
+
+    function timeWast(daily){
+      const monthlyTimeWast = daily * 30;
+      console.log(monthlyTimeWast);
+    }
+  
+    timeWast(2);
+  
+  function tenTimes( number ){
+    const result = number * 10;
+    return result;
+  }
+  const outPut = tenTimes(50);
+  console.log(outPut);
+
+  function add( price1, price2 ){
+    return price1 + price2;
+    // const added = price1 + price2;
+    // return added;
+  }
+
+  const bill = add(50, 50);
+  console.log(bill);
+
+    const potato = 35;
+    const onion = 62;
+    const shirt = 500;
+    const pant = 1250;
+    function add (price1, price2){
+      return price1 + price2;
+    }
+  const bill1 = add(potato, onion);
+  const bill2 = add(shirt, pant);
+  const final = bill1 + bill2;
+    console.log(final);
+    */
+  function doMath( num1, num2 ){
+    const sum = num1 + num2;
+    const diff = num1 - num2;
+    const multipy = sum * diff;
+    const result = multipy / 2;
+    return result
+  }
+
+  const final = doMath(6, 4);
+  console.log(final);

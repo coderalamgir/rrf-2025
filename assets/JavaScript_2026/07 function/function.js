a54d0bf7b5266c221ff4b89613a114fcc606640b
Alamgir hossain <alamgirabc6@gmail.com>
2026-04-13 13:52:39 +0600

    Update function.js

--- assets/JavaScript_2026/07 function/function.js
+++ assets/JavaScript_2026/07 function/function.js
@@ -13,12 +13,12 @@ const nums = [54, 62, 12, 6];
 const result = sumOfNumbers(nums);
 console.log( "Sum of numbers is: " + result);
 
-*/
+
 // একটা array এর শুধুমাত্র জোড় ইলেমেন্টের যোগফল আউটপুটে দেখাতে হবে।
 function onlyOdd(numbers){
     let sum = 0;
     for( const number of numbers ){
-        if( number % 2 === 0 ){
+        if( number % 2 === 1 ){
             sum = sum + number;
         }
     }
@@ -28,3 +28,66 @@ function onlyOdd(numbers){
 const nums = [54, 13, 1, 6];
 const total = onlyOdd(nums);
 console.log("শুধুমাত্র বিজোড় সংখ্যাগুলোর যোগফল " + total);
+
+
+// arrow function এর সাহায্যে শুধুমাত্র বিজোর সংখ্যার যোগফল বের কর।
+const addEven = (numbers) => {
+    let sum = 0;
+    for( const number of numbers ){
+        if(number % 2 === 1){
+            sum = sum + number;
+        }
+    }
+    return sum;
+}
+
+const nums = [5, 15, 8, 7, 2];
+const result = addEven(nums);
+console.log( result);
+
+
+
+const addEven = (numbers) => {
+    let sum = 0;
+    for( const number of numbers ){
+        if(number % 4 === 0){
+            sum = sum + number;
+        }
+    }
+    return sum;
+}
+
+const nums = [2, 4, 5, 7, 8, 32, 45,];
+const result = addEven(nums);
+console.log( result);
+
+
+function multiplyOrDivided(value){
+    if(value < 20){
+        const result = value * 2;
+        console.log(result);
+    }else{
+        const result = value / 20;
+        console.log(result);
+    }
+}
+
+multiplyOrDivided(1000);
+
+
+
+const addEven = (numbers) => {
+    let sum = 0;
+    for( const number of numbers ){
+        if(number % 3 === 0){
+            sum = sum + number;
+        }
+    }
+    return sum;
+}
+
+const nums = [2, 4, 5, 7, 8, 32, 45,];
+const result = addEven(nums);
+console.log( result);
+*/
+
